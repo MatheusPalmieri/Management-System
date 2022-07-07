17f27e81ffbf8f2b1ef69989d55983f949001926
Matheus Palmieri <matheusandrepp@gmail.com>
2022-07-07 12:54:22 -0300

    Initial

---
+++ connection.php
@@ -0,0 +1,12 @@
+<?php
+    $host = 'Localhost';
+    $username = 'root';
+    $password = '';
+    $dataBase = 'desktopsystem';
+
+    $mysqli = new mysqli($host, $username, $password, $dataBase);
+
+    if($mysqli->error) {
+        die('Falha ao conectar ao banco de dados: ' . $mysqli->error);
+    }
+?>
